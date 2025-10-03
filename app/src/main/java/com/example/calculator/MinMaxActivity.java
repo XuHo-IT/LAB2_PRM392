b70e879e3393ade899e2af65c53ef01d0311041f
XuHo-IT <ngotranxuanhoa09062004@gmail.com>
2025-10-03 12:51:59 +0700

    fix Min_Max logic

--- app/src/main/java/com/example/calculator/MinMaxActivity.java
+++ app/src/main/java/com/example/calculator/MinMaxActivity.java
@@ -7,8 +7,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MinMaxActivity extends AppCompatActivity {
-    EditText edNumberA, edNumberB;
+    EditText edNumberList;
     TextView tvResult;
     Button btnMin, btnMax,btnBack;
 
@@ -17,24 +21,49 @@ public class MinMaxActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minmax);
 
-        edNumberA = findViewById(R.id.edNumberA);
-        edNumberB = findViewById(R.id.edNumberB);
+        edNumberList = findViewById(R.id.edNumberList);
         tvResult = findViewById(R.id.tvResult);
         btnMin = findViewById(R.id.btnMin);
         btnMax = findViewById(R.id.btnMax);
         btnBack = findViewById(R.id.btnBack);
         btnMin.setOnClickListener(v -> {
-            double a = Double.parseDouble(edNumberA.getText().toString());
-            double b = Double.parseDouble(edNumberB.getText().toString());
-            tvResult.setText("Min: " + Math.min(a, b));
+            ArrayList<Double> numbers = parseInput();
+            if( !numbers.isEmpty()){
+                double min = Collections.min(numbers);
+                tvResult.setText("Min: "+min);
+            }else{
+                tvResult.setText("Please Enter Numbers");
+            }
+
         });
 
         btnMax.setOnClickListener(v -> {
-            double a = Double.parseDouble(edNumberA.getText().toString());
-            double b = Double.parseDouble(edNumberB.getText().toString());
-            tvResult.setText("Max: " + Math.max(a, b));
+            ArrayList<Double> numbers = parseInput();
+            if( !numbers.isEmpty()){
+                double max = Collections.max(numbers);
+                tvResult.setText("Max: "+max);
+            }else{
+                tvResult.setText("Please Enter Numbers");
+            }
+
         });
         btnBack.setOnClickListener(v -> finish());
 
     }
+    private ArrayList<Double> parseInput(){
+          ArrayList<Double> numbers = new ArrayList<>();
+          String input = edNumberList.getText().toString().trim();
+          if(!input.isEmpty()){
+              String[] parts = input.split("[,\\s]+");
+              for(String part: parts){
+                  try{
+                      numbers.add(Double.parseDouble(part));
+                  }catch (NumberFormatException e){
+
+                  }
+              }
+          }
+          return  numbers;
+    }
+
 }
